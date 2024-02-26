1e7420051ff6b085b1ec01475486be4403d88469
WeronikaGRIG <veronikaudot@gmail.com>
2024-02-26 22:00:15 +0100

    add vocab List

---
+++ src/components/PagesRules/Simple/PresentSimple/PresentSimple.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PresentSimple() {
+    return (
+        <main>
+            <h1>Present Simple</h1>
+            <p>Простое настоящее время</p>
+        </main>
+    )
+}
