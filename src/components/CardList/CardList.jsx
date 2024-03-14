1febbf9f37ac0520ca369ce60941fd072c392d4c
WeronikaGRIG <veronikaudot@gmail.com>
2024-03-14 20:31:23 +0100

    add images

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,9 +1,8 @@
 
-
-export default function CardList({ title, description }) {
+export default function CardList({ title, description, img }) {
     return (
         <li className="card-list">
-            {/* <img src="" alt="#" /> */}
+            <img className="card-list__img" src={img} alt={title} />
             <h2 className="card-list__title">{title}</h2>
             <p className="card-list__descr">{description}</p>
         </li>
