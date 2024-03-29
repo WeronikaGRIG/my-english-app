5943a376f303601ff74fc8543b03bf5a88815c48
WeronikaGRIG <veronikaudot@gmail.com>
2024-03-29 21:38:04 +0100

    add changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,12 +16,6 @@ export default function Home() {
                             <h3 className={styles.list__item_title}>Выбери технику изучения</h3>
                             <p className={styles.list__item_text}>Выбери свой уровень и технику, которую будешь сегодня изучать. </p>
                         </div>
-                        {/* вынести кнопки в отдельный компонент и назвать одинаково стили*/}
-                        <div className={styles.levels}>
-                            <button className={styles.levels__btn}>Начинающий</button>
-                            <button className={styles.levels__btn}>Средний</button>
-                            <button className={styles.levels__btn}>Продвинутый</button>
-                        </div>
                         {/* вынести карточки в отдельный компонент, не забыть про импорты*/}
                         <Card />
                         {/* ДОМАШНЕЕ ЗАДАНИЕ 24week (перенести в отдельную папку и добавить пропсы) */}
