b2708bc77209d38784f6f7e46f5d9ecd7b7d7a01
WeronikaGRIG <veronikaudot@gmail.com>
2024-04-06 17:02:29 +0200

    not changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './styles/reset.css';
 import './styles/variables.css';
 
 import Footer from './components/Footer/Footer';
-// import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Vocab from './components/Vocab/Vocab';
