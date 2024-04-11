d472ca3295f91e6deb4e0088025cb8890166d83c
WeronikaGRIG <veronikaudot@gmail.com>
2024-04-11 15:18:55 +0200

    add state newWord in tables

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,11 +1,61 @@
-
+import { useState } from 'react';
 import TableList from '../TableList/TableList';
 import { list } from '../../list';
 import styles from './Table.module.css';
 import TableHeader from '../TableHeader/TableHeader';
 import TableRowEditor from '../TableRowEditor/TableRowEditor';
 
+
 export default function Table() {
+
+    const [isEditing, setEditing] = useState(false);
+    const [editedFields, setEditedFields] = useState({});
+    const [list, setList] = useState([]);
+    const [newWord, setNewWord] = useState({ word: '', transcription: '', translation: '' });
+    const [errors, setErrors] = useState({});
+
+    const handleEdit = () => {
+        setEditing(true);
+    };
+
+    const handleCancelEdit = () => {
+        setEditing(false);
+        setEditedFields({});
+        setErrors({});
+    };
+
+    const handleFiedChange = (fieldName, value) => {
+        setEditedFields(prevState => ({
+            ...prevState,
+            [fieldName]: value
+        }));
+        setErrors(prevErrors => ({
+            ...prevErrors,
+            [fieldName]: value.trim() === ''
+        }));
+    };
+
+    const handleSave = () => {
+        if (Object.values(errors).some(error => error)) {
+            console.log('Ошибка: Не все поля заполнены.');
+            return;
+        }
+        // Обработка сохранения изменений
+        console.log('Сохраненные данные:', editedFields);
+        setEditing(false);
+        setEditedFields({});
+        setErrors({});
+    };
+
+    const handleAddWord = () => {
+        if (Object.values(errors).some(error => error)) {
+            console.log('Ошибка: Не все поля заполнены.');
+            return;
+        }
+        setList(prevList => [...prevList, newWord]);
+        setNewWord({ word: '', transcription: '', translation: '' })
+    }
+
     return (
 
         <main className={styles.main}>
@@ -15,14 +65,35 @@ export default function Table() {
                     <table className={styles.table}>
                         <tbody className={styles.tbody}>
                             <TableHeader />
-                            <TableRowEditor />
-                            {list.map((table, i) => {
+
+                            {isEditing ? (
+                                <TableRowEditor
+                                    editedFields={editedFields}
+                                    onCancelEdit={handleCancelEdit}
+                                    onFieldChange={handleFiedChange}
+                                    onSave={handleSave}
+                                    newWord={newWord}
+                                    setNewWord={setNewWord} />
+                            ) : null}
+
+                            {list.map((word, index) => {
                                 return (
-                                    <TableList key={i} {...table} />
+                                    <TableList key={index} {...word} />
                                 )
                             })}
                         </tbody>
                     </table>
+                    {!isEditing && (
+                        <div className={styles.button}>
+                            <button className={styles.td__btn} onClick={handleEdit}>Добавить слово</button>
+                        </div>
+                    )}
+                    {isEditing && (
+                        <div className={styles.button}>
+                            <button className={styles.td__btn} onClick={handleAddWord}>Добавить слово</button>
+                        </div>
+                    )}
+
                 </div>
             </div>
         </main>
--- src/components/TableRowEditor/TableRowEditor.jsx
+++ src/components/TableRowEditor/TableRowEditor.jsx
@@ -1,22 +1,71 @@
+import { useState } from 'react';
 import styles from './TableRowEditor.module.css'
 
-export default function TableRowEditor() {
+export default function TableRowEditor({ editedFields, setEditedFields, onCancelEdit, onFieldChange, onSave }) {
+    const [errors, setErrors] = useState({});
+    const [isEmptyField, setEmptyField] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === 'word' || name === 'transcription' || name === 'translation') {
+            onFieldChange(name, value);
+            setEditedFields(prevFields => ({ ...prevFields, [name]: value }));
+        }
+
+        const isEmpty = value.trim() === '';
+
+        setErrors(prevErrors => ({
+            ...prevErrors,
+            [name]: isEmpty
+        }));
+
+        setEmptyField(isEmpty);
+    };
+
+    const handleSave = () => {
+        if (isEmptyField) {
+            console.log('Ошибка: Не все поля заполнены.');
+            return;
+        }
+        onSave();
+    };
+
+
     return (
         <tr className={styles.tr__edit}>
             <td className={styles.td__edit}>
-                <input type="text" name="text" className={styles.td__input} pattern="\d+" placeholder="" />
+                <input
+                    type="text"
+                    name="word"
+                    className={`${styles.td__input} ${errors.word ? styles.input__error : ''}`}
+                    value={editedFields.word || ''}
+                    onChange={handleChange} />
             </td>
             <td className={styles.td__edit}>
-                <input type="text" name="text" className={styles.td__input} pattern="\d+" placeholder="" />
+                <input
+                    type="text"
+                    name="transcription"
+                    className={`${styles.td__input} ${errors.transcription ? styles.input__error : ''}`}
+                    value={editedFields.transcription || ''}
+                    onChange={handleChange} />
             </td>
             <td className={styles.td__edit}>
-                <input type="text" name="text" className={styles.td__input} pattern="\d+" placeholder="" />
+                <input type="text"
+                    name="translation"
+                    className={`${styles.td__input} ${errors.translation ? styles.input__error : ''}`}
+                    value={editedFields.translation || ''}
+                    onChange={handleChange} />
             </td>
 
-            {/* вынести в отдельные компоненты кнопки */}
-            <div className={styles.button}>
-                <button className={styles.td__btn}>Добавить слово</button>
-            </div>
+            <td className={styles.button}>
+
+                <button className={styles.td__btn}
+                    onClick={handleSave}
+                    disabled={Object.values(errors).some(error => error)}>Сохранить</button>
+                <button className={styles.td__btn}
+                    onClick={onCancelEdit}>Отмена</button>
+
+            </td>
         </tr>
     )
 }
