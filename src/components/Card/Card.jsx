a9cf9d1169b9d5379198d8edf5c640dad251cdd6
WeronikaGRIG <veronikaudot@gmail.com>
2024-05-22 17:11:30 +0200

    add new branch, library axios, ContextWord,

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,25 @@
-import { data } from '../../data.js';
+
+
+import { useContext } from 'react';
+import { WordContext } from '../../context/WordContext.jsx';
+// import { data } from '../../data.js';
 
 import CardList from '../CardList/CardList.jsx';
 
 import styles from './Card.module.css';
 
+
 export default function Card() {
+
+    const { words } = useContext(WordContext)
+
     return (
         <div className={styles.container}>
 
             <ul className={styles.card}>
-                {data.map((card, i) => {
+                {words.map((word, id) => {
                     return (
-                        <CardList key={i} {...card} />
+                        <CardList key={id} {...word} />
                     )
                 })}
             </ul>
